96ffd0d53cb183bef5e2deb2c182b490055abf5f
chintha phanindra <chintha.phanindra40@gmail.com>
2025-07-14 19:25:05 +0530

    middleware changes

--- config/middlewares.js
+++ config/middlewares.js
@@ -3,7 +3,7 @@ module.exports = [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:5173', 'https://vitaforge.netlify.app/'], // Add your frontend domains
+      origin: ['http://localhost:5173', 'https://vitaforge.netlify.app'], // Add your frontend domains
       credentials: true,
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
       
